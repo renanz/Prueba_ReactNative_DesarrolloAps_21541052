9d3047d8fcfc86791fbba4c0b9a5db8ed71735f1
renanz <renan_zelaya@hotmail.com>
2018-12-10 16:59:26 -0600

    Done, missing some more style

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: [ 'some text', 'some text 2'] };
+    this.state = { textList: [], label: '' };
   }
   
+  onPress = () => {
+    let temp = [];
+    const tempList = this.state.textList;
+    for (let index = 0; index < tempList.length; index += 1) {
+      temp.push(tempList[index]);
+    }
+    temp.push(this.state.label);
+    this.setState({
+        textList: temp,
+        label: ''
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(newText) => this.setState({text})}
-          value={this.state.text}
-        />
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={this.row}>
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+            editable = {true}
+            onChangeText={(text) => this.setState({
+              textList: this.state.textList,
+              label: text
+            })}
+            placeholder='Write a Todo'
+            value={this.state.label}
+          />
+          <Button title="+" onPress={this.onPress}/>
+        </View>
+        {this.state.textList.map(data=> (
+          <Text>{data}</Text>
+        ))}
       </View>
     );
   }
@@ -27,5 +50,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row"
   },
 });
