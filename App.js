999ff4c393cd2a2dd6e6e4f5a6d571503dd28d9a
renanz <renan_zelaya@hotmail.com>
2018-12-10 16:24:10 -0600

    initial

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: [ 'some text', 'some text 2'] };
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(newText) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
